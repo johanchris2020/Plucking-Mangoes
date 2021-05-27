7d3a2067153684b226725e3f15761f5ed8cc38f7
johanchris2020 <71026064+johanchris2020@users.noreply.github.com>
2021-05-27 15:32:32 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -4,14 +4,14 @@ class tree
 	{
 		this.x=x;
 		this.y=y;
-		this.dustbinWidth=450;
-		this.dustbinHeight=600;
+		this.width = 450;
+		this.height =600;
 		this.wallThickness=10;
 		
 		this.image=loadImage("tree.png")
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-		this.leftWallBody=Bodies.rectangle(0, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.width, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(0, this.y-this.height/2, this.wallThickness, this.height, {isStatic:false})
+		this.rightWallBody=Bodies.rectangle(this.x+this.width/2, this.y-this.height/2, this.wallThickness, this.height, {isStatic:false})
 		
 		World.add(world, this.bottomBody)
 		World.add(world, this.leftWallBody)
@@ -25,7 +25,7 @@ class tree
 			translate(posBottom.x, posBottom.y+10);
 			fill(255)
 			imageMode(CENTER);
-			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			image(this.image, 0,-this.height/2,this.width, this.height)
 			pop()
 			
 	}
